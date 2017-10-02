cbd0410eb7e9b2c1b6cef63188e4b2d95c36245d
91071346 <31664270+91071346@users.noreply.github.com>
2017-10-02 10:28:13 -0700

    initial commit

--- src/org/teachingkidsprogramming/section03ifs/HiLow2.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow2.java
@@ -5,13 +5,14 @@ import java.util.Random;
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow
+//this variation tells the user how many guesses they have left
+public class HiLow2
 {
   public static void main(String[] args)
   {
     Random rand = new Random();
     int answer = rand.nextInt(100) + 1;
-    //int answer = (int) (Math.random() * 100 + 1);
+    //int answer = (int) (Math.random() * 100 + 1); 
     for (int i = 1; i <= 8; i++)
     {
       int guess = MessageBox.askForNumericalInput("What is your guess?");
@@ -29,6 +30,7 @@ public class HiLow
       {
         MessageBox.showMessage("Too low");
       }
+      MessageBox.showMessage("you have" + "" + (8 - i) + " " + "guesses left");
       if (i == 8)
       {
         MessageBox.showMessage("You lost");
