9a57012d4d275264d073649bad4640a6aae4c25e
91071346 <31664270+91071346@users.noreply.github.com>
2017-10-10 09:15:20 -0700

    initial commit

---
+++ src/org/teachingkidsprogramming/section03ifs/HiLow3.java
@@ -0,0 +1,46 @@
+package org.teachingkidsprogramming.section03ifs;
+
+import java.util.Random;
+
+import org.teachingextensions.logo.Sound;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
+//this variation tells the user how many guesses they have left
+public class HiLow3
+{
+  public static void main(String[] args)
+  {
+    Random rand = new Random();
+    int answer = rand.nextInt(100) + 1;
+    //int answer = (int) (Math.random() * 100 + 1); 
+    int upperRange = 100;
+    for (int i = 1; i <= 8; i++)
+    {
+      int guess = MessageBox.askForNumericalInput("What is your guess?");
+      while (guess < 1 || guess > upperRange)
+      {
+        MessageBox.showMessage("Please enter a valid guess between 1 and " + upperRange);
+        guess = MessageBox.askForNumericalInput("What is your guess?");
+      }
+      if (guess == answer)
+      {
+        Sound.playBeep();
+        MessageBox.showMessage("You won!!");
+        break;
+      }
+      else if (guess > answer)
+      {
+        MessageBox.showMessage("Too high");
+      }
+      else
+      {
+        MessageBox.showMessage("Too low");
+      }
+      MessageBox.showMessage("you have" + " " + (8 - i) + " " + "guesses left");
+      if (i == 8)
+      {
+        MessageBox.showMessage("You lost");
+      }
+    }
+  }
+}
