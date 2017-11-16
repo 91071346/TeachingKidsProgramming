43d22a65b6e76f6e7b70a62317ae3b8ee91bc5e0
91071346 <31664270+91071346@users.noreply.github.com>
2017-11-16 14:31:39 -0800

    Started Spider_Web

--- src/org/teachingkidsprogramming/section05recursion/RecursiveSquare2.java
+++ src/org/teachingkidsprogramming/section05recursion/RecursiveSquare2.java
@@ -17,29 +17,21 @@ public class RecursiveSquare2
   }
   private static void makeASquare(double length)
   {
-    //  If the current length is greater than 10 --#10.2
     if (length > 10)
     {
-      //      Run the recipe moveToTheSquareStart with the current length  --#4.3
       moveToTheSquareStart(length);
       for (int i = 0; i < 4; i++)
       {
         Tortoise.move(length);
-        //    MakeASquare with the current length divided by 1.7 (recipe below)--#11.3 
         makeASquare(length / 1.7);
-        //      If the current process count is less than 3 (HINT: use 'i') --#9
         if (i < 3)
         {
           Tortoise.turn(90);
         }
       }
     }
-    //  MoveBackToCenter with the current length (recipe below)--#5.3
     moveBackToCenter(length);
-    //
     length = length * 2;
-    //
-    //  End of makeASquare recipe --#11.2
   }
   private static void moveBackToCenter(double length)
   {
