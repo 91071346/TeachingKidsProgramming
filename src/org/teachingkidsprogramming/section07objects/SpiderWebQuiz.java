53791c783f5c0db8e2b858b4a320ed760a701057
91071346 <31664270+91071346@users.noreply.github.com>
2017-12-07 14:31:07 -0800

    Finished FizzBuzz and FizzBuzzTTD and its tests

--- src/org/teachingkidsprogramming/section07objects/SpiderWebQuiz.java
+++ src/org/teachingkidsprogramming/section07objects/SpiderWebQuiz.java
@@ -2,7 +2,7 @@ package org.teachingkidsprogramming.section07objects;
 
 import org.teachingkidsprogramming.recipes.quizzes.graders.SpiderQuiz;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SpiderWebQuizGrader;
-public class circleAround
+
 public class SpiderWebQuiz extends SpiderQuiz
 {
   @Override
@@ -15,33 +15,41 @@ public class SpiderWebQuiz extends SpiderQuiz
   }
   public void question2()
   {
-    //      Create and then call a recipe called circleAround which 
-    {
-     for (int i = 0; i < 3; i++)
+    circleAround();
+  }
+  public void circleAround()
+  {
+    for (int i = 0; i < 3; i++)
     {
       adjust();
-      circle();
-    } 
+      question1();
     }
-    //      Does the following 3 times
-    //      Call adjust()
-    //      Call question1
-    //      Repeat
   }
   public void question3()
   {
-    //      Create and then call a recipe called grow which 
-    //      Changes the current length so it is multiplied by 2.5
+    grow();
+  }
+  public void grow()
+  {
+    {
+      length = length * 2.5;
+    }
   }
   public void question4()
   {
-    //      Create and then call a recipe called shrink which 
-    //      Decreases the current length by 9 pixels
+    shrink();
+  }
+  public void shrink()
+  {
+    length = length - 9;
   }
   public void question5()
   {
-    //      Create and then call a recipe called expand which
-    //      Increases the current number by 12
+    expand();
+  }
+  public void expand()
+  {
+    number = number + 12;
   }
   public static void main(String[] args)
   {
